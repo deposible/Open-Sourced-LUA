533a58a4cf8571cfaf84135f0e609c9c8956eead
Deposible <141058672+deposible@users.noreply.github.com>
2026-01-26 18:57:39 -0800

    Create Click To Aura Farm (128120317905952).lua

---
+++ Scripts/Click To Aura Farm (128120317905952).lua
@@ -0,0 +1,60 @@
+-- both 128120317905952 & 116530775830497
+getgenv().Settings = {
+    Delay = 0.1,
+    AutoRaceFinish = false,
+    AutoClaimGifts = false,
+    BuyPaddleTillRarity = false,
+    PaddleRarities = {"Epic","Legendary","Secret"},
+    BuyBoatTillRarity = false,
+    BoatRarities = {"Epic","Legendary","Secret"}
+}
+
+if Settings then return end
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+task.spawn(function()
+    while task.wait(Settings.Delay) do
+        if Settings.AutoRaceFinish then
+            local gui = LocalPlayer.PlayerGui
+            if gui
+            and gui:FindFirstChild("ProgressBar")
+            and gui.ProgressBar.TopHUD.ProgressBar.Timer.Visible then
+                game.ReplicatedStorage.src.Packages.Knit.Services.GameService.RE.HandleClick:FireServer(3)
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(Settings.Delay) do
+        if Settings.AutoClaimGifts then
+            for i = 1, 9 do
+                game.ReplicatedStorage.src.Packages.Knit.Services.RewardService.RF.ClaimTimeReward:InvokeServer(i)
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(1) do
+        if Settings.BuyPaddleTillRarity then
+            local r = game.ReplicatedStorage.src.Packages.Knit.Services.SpinAuraService.RF.Spin:InvokeServer("Paddle","Normal")
+            if table.find(Settings.PaddleRarities, r) then
+                Settings.BuyPaddleTillRarity = false
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    while task.wait(1) do
+        if Settings.BuyBoatTillRarity then
+            local r = game.ReplicatedStorage.src.Packages.Knit.Services.SpinAuraService.RF.Spin:InvokeServer("Boat","Normal")
+            if table.find(Settings.BoatRarities, r) then
+                Settings.BuyBoatTillRarity = false
+            end
+        end
+    end
+end)
